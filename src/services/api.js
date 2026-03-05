fa97a97bbf7ccbf2eac4c820c13f3ffd43751a21
tejash0901 <tejash0901@gmail.com>
2026-03-05 12:23:24 +0530

    Prepare frontend for hosted backend API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api',
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    'https://community-sharing-backend.onrender.com/api',
 })
 
 api.interceptors.request.use((config) => {
